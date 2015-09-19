f654b66db222c6d0be9d00e136d84cacfc78e088
Tim Cameron Ryan <id@timryan.org>
2015-09-19 14:15:59 -0700

    Removes static assets.

--- tools/publish-site.sh
+++ tools/publish-site.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+set -e
+
 cd $(dirname $0)/..
 make 
 git add -f static/*
 git commit -am "Publishes site update."
 git push origin `git subtree split --prefix static master`:refs/heads/gh-pages --force
+git reset HEAD~1
