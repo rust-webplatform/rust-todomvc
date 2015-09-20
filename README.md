9b5a9e523250eb88370594f87812659ad93de129
Tim Cameron Ryan <id@timryan.org>
2015-09-20 13:31:08 -0700

    Implements more features according to app spec!

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # rust-todomvc
 
 See [rust-webplatform](https://github.com/tcr/rust-webplatform) for more details.
+
+See [the TodoMVC app spec](https://github.com/tastejs/todomvc/blob/master/app-spec.md).
+
+## license
+
+MIT/Apache-2.0
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ extern crate mustache;
 extern crate rustc_serialize;
 
 use mustache::{MapBuilder};
+use std::rc::Rc;
+use std::cell::RefCell;
+use webplatform::Event;
 
 const INIT_HTML:&'static str = r##"
 <title>Rust &middot; TodoMVC</title>
@@ -46,6 +49,11 @@ const INIT_HTML:&'static str = r##"
 </footer>
 "##;
 
+struct TodoItem {
+    content: String,
+    completed: bool,
+}
+
 fn main() {
     let document = webplatform::init();
 
@@ -62,35 +70,89 @@ fn main() {
 </li>"#);
 
     let todo_new = document.element_query(".new-todo").unwrap();
+    let todo_count = document.element_query(".todo-count").unwrap();
     let list = document.element_query(".todo-list").unwrap();
     let clear = document.element_query(".clear-completed").unwrap();
 
-    clear.on("click", move || {
-        webplatform::alert("TODO");
+    let mut itemslist:Rc<RefCell<Vec<TodoItem>>> = Rc::new(RefCell::new(vec![]));
+
+    let iref = itemslist.clone();
+    let llist = list.root_ref();
+    let render = Rc::new(move || {
+        let mut items = iref.borrow_mut();
+
+        llist.html_set("");
+
+        for (i, item) in items.iter().enumerate() {
+            let data = MapBuilder::new()
+                .insert_str("id", format!("{}", i))
+                .insert_str("checked", if item.completed { "checked" } else { "" })
+                .insert_str("value", item.content.clone())
+                .build();
+
+            let mut vec = Vec::new();
+            template.render_data(&mut vec, &data);
+            llist.html_append(&String::from_utf8(vec).unwrap());
+        }
+
+        let len = items.iter().filter(|&x| !x.completed).count();
+        let leftstr = if len == 1 {
+            "<strong>1</strong> item left.".to_string()
+        } else {
+            format!("<strong>{}</strong> items left.", len)
+        };
+        todo_count.html_set(&leftstr);
+
+        document.element_query(".main").unwrap().style_set_str("display", if items.len() == 0 { "none" } else { "block" });
+        document.element_query(".footer").unwrap().style_set_str("display", if items.len() == 0 { "none" } else { "block" });
+    });
+
+    let iref = itemslist.clone();
+    let rrender = render.clone();
+    list.on("click", move |e:Event| {
+        let node = e.target.unwrap();
+        if node.class_get().contains("destroy") {
+            let id = node.parent().unwrap().parent().unwrap().data_get("id").unwrap().parse::<usize>().unwrap();
+            iref.borrow_mut().remove(id);
+            rrender();
+        } else if node.class_get().contains("toggle") {
+            let id = node.parent().unwrap().parent().unwrap().data_get("id").unwrap().parse::<usize>().unwrap();
+            {
+                let item = &mut iref.borrow_mut()[id];
+                item.completed = !item.completed;
+            }
+            rrender();
+        }
+    });
+
+    let iref = itemslist.clone();
+    let rrender = render.clone();
+    clear.on("click", move |_:Event| {
+        iref.borrow_mut().retain(|ref x| !x.completed);
+        rrender();
     });
 
     let t1 = todo_new.root_ref();
-    // let d1 = document.clone();
-    todo_new.on("change", move || {
-        let value = t1.prop_get_str("value");
+    let iref = itemslist.clone();
+    let rrender = render.clone();
+    todo_new.on("change", move |_:Event| {
+        {
+            let mut items = iref.borrow_mut();
 
-        let data = MapBuilder::new()
-            .insert_str("id", "0")
-            .insert_str("checked", "")
-            .insert_str("value", value)
-            .build();
+            let value = t1.prop_get_str("value");
+            t1.prop_set_str("value", "");
 
-        let mut vec = Vec::new();
-        template.render_data(&mut vec, &data);
+            let item = TodoItem {
+                content: value,
+                completed: false,
+            };
+            items.push(item);
+        }
 
-        list.html_append(&String::from_utf8(vec).unwrap());
-
-        let entry = document.element_query(".todo-list li:last-child").unwrap();
-        let entry_delete = document.element_query(".todo-list li:last-child button").unwrap();
-        entry_delete.on("click", move || {
-            entry.remove_self();
-        });
+        rrender();
     });
+
+    render();
 
     webplatform::spin();
 }
