8c512ca8a2eb69c931fdbb15e05bcf62f973792a
Tim Cameron Ryan <id@timryan.org>
2015-09-20 18:54:53 -0700

    Implements clear-all.

--- src/main.rs
+++ src/main.rs
@@ -5,53 +5,26 @@
 extern crate libc;
 extern crate mustache;
 extern crate rustc_serialize;
+extern crate webplatform_url;
 
 use mustache::{MapBuilder};
 use std::rc::Rc;
-use std::cell::RefCell;
+use std::cell::{RefCell, Cell};
 use webplatform::Event;
+use webplatform_url::parse_path;
 
-const INIT_HTML:&'static str = r##"
-<title>Rust &middot; TodoMVC</title>
-<link rel="stylesheet" href="base.css">
-<link rel="stylesheet" href="index.css">
-<section class="todoapp">
-    <header class="header">
-        <h1>todos</h1>
-        <input class="new-todo" placeholder="What needs to be done?" autofocus>
-    </header>
-    <section class="main">
-        <input class="toggle-all" type="checkbox">
-        <label for="toggle-all">Mark all as complete</label>
-        <ul class="todo-list"></ul>
-    </section>
-    <footer class="footer">
-        <span class="todo-count"></span>
-        <ul class="filters">
-            <li>
-                <a href="#/" class="selected">All</a>
-            </li>
-            <li>
-                <a href="#/active">Active</a>
-            </li>
-            <li>
-                <a href="#/completed">Completed</a>
-            </li>
-        </ul>
-        <button class="clear-completed">Clear completed</button>
-    </footer>
-</section>
-<footer class="info">
-    <p>Double-click to edit a todo</p>
-    <p>Created by <a href="http://twitter.com/oscargodson">Oscar Godson</a></p>
-    <p>Refactored by <a href="https://github.com/cburgmer">Christoph Burgmer</a></p>
-    <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-</footer>
-"##;
+const INIT_HTML: &'static str = include_str!("template-page.html");
 
 struct TodoItem {
     content: String,
     completed: bool,
+}
+
+#[derive(Copy)]
+enum TodoState {
+    Active,
+    Completed,
+    All
 }
 
 fn main() {
@@ -60,15 +33,6 @@ fn main() {
     let body = document.element_query("body").unwrap();
     body.html_set(INIT_HTML);
 
-    let template = mustache::compile_str(r#"
-<li data-id="{{id}}" class="{{completed}}">
-  <div class="view">
-    <input class="toggle" type="checkbox" {{checked}}>
-    <label>{{value}}</label>
-    <button class="destroy"></button>
-  </div>
-</li>"#);
-
     let todo_new = document.element_query(".new-todo").unwrap();
     let todo_count = document.element_query(".todo-count").unwrap();
     let list = document.element_query(".todo-list").unwrap();
@@ -76,16 +40,32 @@ fn main() {
     let main = document.element_query(".main").unwrap();
     let footer = document.element_query(".footer").unwrap();
 
-    let itemslist:Rc<RefCell<Vec<TodoItem>>> = Rc::new(RefCell::new(vec![]));
+    let filter_all = document.element_query(".filters li:nth-child(1) a").unwrap();
+    let filter_active = document.element_query(".filters li:nth-child(2) a").unwrap();
+    let filter_completed = document.element_query(".filters li:nth-child(3) a").unwrap();
+
+    let toggle_all = document.element_query(".toggle-all").unwrap();
+
+    let state = Rc::new(Cell::new(TodoState::All));
+    let itemslist: Rc<RefCell<Vec<TodoItem>>> = Rc::new(RefCell::new(vec![]));
+
+    let template = mustache::compile_str(include_str!("template-todo.html"));
 
     let iref = itemslist.clone();
     let llist = list.root_ref();
+    let sstate = state.clone();
     let render = Rc::new(move || {
         let items = iref.borrow_mut();
 
         llist.html_set("");
 
-        for (i, item) in items.iter().enumerate() {
+        for (i, item) in items.iter().filter(|&x| {
+            match sstate.get() {
+                TodoState::All => true,
+                TodoState::Active => !x.completed,
+                TodoState::Completed => x.completed,
+            }
+        }).enumerate() {
             let data = MapBuilder::new()
                 .insert_str("id", format!("{}", i))
                 .insert_str("checked", if item.completed { "checked" } else { "" })
@@ -107,6 +87,25 @@ fn main() {
 
         main.style_set_str("display", if items.len() == 0 { "none" } else { "block" });
         footer.style_set_str("display", if items.len() == 0 { "none" } else { "block" });
+
+
+        match sstate.get() {
+            TodoState::All => {
+                filter_all.class_add("selected");
+                filter_active.class_remove("selected");
+                filter_completed.class_remove("selected");
+            },
+            TodoState::Active => {
+                filter_all.class_remove("selected");
+                filter_active.class_add("selected");
+                filter_completed.class_remove("selected");
+            },
+            TodoState::Completed => {
+                filter_all.class_remove("selected");
+                filter_active.class_remove("selected");
+                filter_completed.class_add("selected");
+            },
+        }
     });
 
     let iref = itemslist.clone();
@@ -134,27 +133,57 @@ fn main() {
         rrender();
     });
 
-    document.on("hashchange", |_:Event| {
-        println!("hash changed. {}", &document.location_hash_get()[1..]);
-    });
-
     let t1 = todo_new.root_ref();
     let iref = itemslist.clone();
     let rrender = render.clone();
     todo_new.on("change", move |_:Event| {
-        {
-            let mut items = iref.borrow_mut();
+        let value = t1.prop_get_str("value");
+        t1.prop_set_str("value", "");
 
-            let value = t1.prop_get_str("value");
-            t1.prop_set_str("value", "");
+        iref.borrow_mut().push(TodoItem {
+            content: value,
+            completed: false,
+        });
+        rrender();
+    });
 
-            let item = TodoItem {
-                content: value,
-                completed: false,
-            };
-            items.push(item);
+    let ddoc = Rc::new(document);
+
+    let rrender = render.clone();
+    let sstate = state.clone();
+    let ddoc2 = ddoc.clone();
+    let update_path = Rc::new(move || {
+        let hash = ddoc2.location_hash_get();
+        let path = if hash.len() < 1 {
+            vec!["".to_string()]
+        } else {
+            parse_path(&hash[1..]).unwrap().0
+        };
+        println!("hash changed. {:?}", path);
+
+        match &*path[0] {
+            "active" => sstate.set(TodoState::Active),
+            "completed" => sstate.set(TodoState::Completed),
+            _ => sstate.set(TodoState::All),
         }
 
+        rrender();
+    });
+
+    let upath = update_path.clone();
+    ddoc.on("hashchange", move |_:Event| {
+        upath();
+    });
+    update_path();
+
+    let rrender = render.clone();
+    let tgl = toggle_all.root_ref();
+    let iref = itemslist.clone();
+    toggle_all.on("change", move |_:Event| {
+        let val = if tgl.prop_get_i32("checked") == 1 { true } else { false };
+        for item in iref.borrow_mut().iter_mut() {
+            item.completed = val;
+        }
         rrender();
     });
 
